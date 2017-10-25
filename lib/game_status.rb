77d7fece56e60f907fe8a8d11ecad53d213ce398
bennybop <bennybop74@yahoo.com>
2017-10-25 00:48:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,4 +18,26 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 winner = nil
+
+WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  position_1 = board[win_index_1]
+
+  win_index_2 = win_combination[1]
+  position_2 = board[win_index_2]
+
+  win_index_3 = win_combination[2]
+  position_3 = board[win_index_3]
+
+if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  winner = win_combination
+
+elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  winner = win_combination
+end
+
+break if winner
+end
+return winner
+
 end
